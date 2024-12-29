8e4a7711b69491b80db7f6dce43fcf7e9c8e4e1a
Cael Yasutake <c.yasutake28@gmail.com>
2024-12-29 04:14:27 -0500

    Added input polling

--- Mochi/src/Mochi/Application.cpp
+++ Mochi/src/Mochi/Application.cpp
@@ -5,6 +5,8 @@
 
 #include <glad/glad.h>
 
+#include "Input.h"
+
 namespace Mochi {
 
 #define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
@@ -53,6 +55,9 @@ namespace Mochi {
 
 			for (Layer* layer : m_LayerStack)
 				layer->OnUpdate();
+
+			auto [x, y] = Input::GetMousePosition();
+			MC_CORE_TRACE("{0}, {1}", x, y);
 
 			m_Window->OnUpdate();
 		}
