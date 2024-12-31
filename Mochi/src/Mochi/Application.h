8c01ce749254b32e4207a42ec7489c3ce99c9ec8
Cael Yasutake <c.yasutake28@gmail.com>
2024-12-31 02:43:44 -0500

    Cleaned up vertex arrays

--- Mochi/src/Mochi/Application.h
+++ Mochi/src/Mochi/Application.h
@@ -11,6 +11,7 @@
 
 #include "Mochi/Renderer/Shader.h"
 #include "Mochi/Renderer/Buffer.h"
+#include "Mochi/Renderer/VertexArray.h"
 
 namespace Mochi {
 	class MOCHI_API Application {
@@ -36,10 +37,11 @@ namespace Mochi {
 		bool m_Running = true;
 		LayerStack m_LayerStack;
 
-		unsigned int m_VertexArray;
-		std::unique_ptr<Shader> m_Shader;
-		std::unique_ptr<VertexBuffer> m_VertexBuffer;
-		std::unique_ptr<IndexBuffer> m_IndexBuffer;
+		std::shared_ptr<Shader> m_Shader;
+		std::shared_ptr<VertexArray> m_VertexArray;
+
+		std::shared_ptr<Shader> m_BlueShader;
+		std::shared_ptr<VertexArray> m_SquareVA;
 	private:
 		static Application* s_Instance;
 	};
