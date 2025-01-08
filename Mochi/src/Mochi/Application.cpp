bcf1f480d89dcaa86fc14c56377cc0e7907a262d
Cael Yasutake <c.yasutake28@gmail.com>
2025-01-08 08:28:10 -0500

    Added blending for textures

--- Mochi/src/Mochi/Application.cpp
+++ Mochi/src/Mochi/Application.cpp
@@ -21,10 +21,9 @@ namespace Mochi {
 
 		m_Window = std::unique_ptr<Window>(Window::Create());
 		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
-		//m_Window->SetVSync(false);
+		
+		Renderer::Init();
 
-		//m_ImGuiLayer = new ImGuiLayer();
-		//PushOverlay(m_ImGuiLayer);
 		m_ImGuiLayer = std::make_unique<ImGuiLayer>();
 	}
 
--- Mochi/src/Mochi/Renderer/RendererAPI.h
+++ Mochi/src/Mochi/Renderer/RendererAPI.h
@@ -11,6 +11,7 @@ namespace Mochi {
 		};
 		
 	public:
+		virtual void Init() = 0;
 		virtual void SetClearColor(const glm::vec4& color) = 0;
 		virtual void Clear() = 0;
 
